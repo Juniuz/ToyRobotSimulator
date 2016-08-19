ae7fbac7f810c6f137531b94e9eb052aa49849db
Juniuz <jdimasuhid@gmail.com>
2016-08-20 11:06:57 +1200

    Enum Types

---
+++ BauerXcelMedia/RobotSimulator/EnumTypes/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BauerXcel.Media.RobotSimulator.EnumTypes
+{
+    /// <summary>
+    /// Determines the instruction given to the toy robot
+    /// </summary>
+    public enum Command : byte
+    {
+        Invalid = 0,
+        Place = 1,
+        Left = 2,
+        Right = 3,
+        Move = 4,
+        Report = 5
+    }
+}
---
+++ BauerXcelMedia/RobotSimulator/EnumTypes/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BauerXcel.Media.RobotSimulator.EnumTypes
+{
+    /// <summary>
+    /// The left and right direction of the toy robot
+    /// </summary>
+    public enum Direction
+    {
+        Left = 1,
+        Right = -1
+    }
+}
